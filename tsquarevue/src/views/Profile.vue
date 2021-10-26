5b309e504844e2a3d16f5d2eb3623db0f1af0823
Jiawei19 <46662273+Jiawei19@users.noreply.github.com>
2021-10-26 13:39:01 +0800

    Footer css in Profile.vue

--- tsquarevue/src/views/Profile.vue
+++ tsquarevue/src/views/Profile.vue
@@ -59,11 +59,20 @@ export default {
 	
 </script>
 
-<style>
+<style scoped>
 #bg { 
     display: block;
     margin-left: auto;
     margin-right: auto;
     width: 60%;
 }
+
+Footer {
+	position: absolute;
+	bottom:0;
+	left:0;
+	width:100%;
+	height:300px;
+}
+
 </style>
