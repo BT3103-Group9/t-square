62ec91df08e35dc81447c5b9b6b1fba7fe305dd5
yibingtan <85612320+yibingtan@users.noreply.github.com>
2021-11-10 10:12:05 +0800

    edited the questions

--- tsquarevue/src/components/Questions.vue
+++ tsquarevue/src/components/Questions.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="container my-5">
-	<h1 class="text-left">General FAQ</h1>
+	<h1 class="text-left">General Frequently Asked Questions</h1>
 	<div class="accordion" id="accordionExample">
 		<!-- Item 1-->
 		<div class="accordion-item">
